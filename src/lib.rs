3118335ab1f6b037e2dc4f73384b03a0ced45450
msharipov <msharipovr@gmail.com>
2024-11-14 21:30:20 -0500

    style: remove 'standard' from function names

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,8 @@ pub struct MapFolding {
 }
 
 impl MapFolding {
-
     /// Attempts to create a 1-dimensional map folding based on a list of segments
-    /// written in the standard stack notation. 
+    /// written in the standard stack notation.
     ///
     /// The standard stack notation requires that:
     /// 1. Segment #1 is facing the same way as it did before the map was folded.
@@ -26,11 +25,11 @@ impl MapFolding {
     /// # Example:
     /// ```
     /// # use map_folding::MapFolding;
-    /// let f = MapFolding::from_standard_stack(&[1, 3, 4, 2]).unwrap();
+    /// let f = MapFolding::from_stack(&[1, 3, 4, 2]).unwrap();
     ///
-    /// assert_eq!(format!("{:?}", f.standard_stack()), "[1, 3, 4, 2]");
+    /// assert_eq!(format!("{:?}", f.stack()), "[1, 3, 4, 2]");
     /// ```
-    pub fn from_standard_stack(stack: &[u64]) -> Result<Self, ()> {
+    pub fn from_stack(stack: &[u64]) -> Result<Self, ()> {
         let indices = Vec::from(stack);
         let n = indices.len();
 
@@ -58,21 +57,22 @@ impl MapFolding {
         }
         Err(())
     }
-    
+
     /// Returns the standard stack representation of the map folding as a list
     /// of segments.
     ///
     /// # Example
     /// ```
     /// # use map_folding::MapFolding;
-    /// let f = MapFolding::from_standard_stack(&[5, 2, 3, 4, 1]).unwrap();
+    /// let f = MapFolding::from_stack(&[5, 2, 3, 4, 1]).unwrap();
     ///
-    /// assert_eq!(f.standard_stack(), [5, 2, 3, 4, 1]);
+    /// assert_eq!(f.stack(), [5, 2, 3, 4, 1]);
     /// ```
-    pub fn standard_stack(&self) -> &[u64] {
+    pub fn stack(&self) -> &[u64] {
         &self.indices[..]
     }
 
+    /// Returns a vector that lists the position of
     pub fn positions(&self) -> Vec<usize> {
         let n = self.indices.len();
         let mut positions: Vec<usize> = vec![0; n + 1]; // 0th value is unused
@@ -94,7 +94,7 @@ mod tests {
     #[test]
     fn from_standard_stack_8_zigzag() {
         let indices = [1, 2, 3, 4, 5, 6, 7, 8];
-        let created = MapFolding::from_standard_stack(&indices).expect("failed to parse indices");
+        let created = MapFolding::from_stack(&indices).expect("failed to parse indices");
         let expected = vec![1, 2, 3, 4, 5, 6, 7, 8];
         assert_eq!(created.indices, expected, "indices do not match");
     }
@@ -102,24 +102,24 @@ mod tests {
     #[test]
     fn from_standard_stack_invalid_indices() {
         let indices = [2, 5, 4, 3];
-        assert_eq!((), MapFolding::from_standard_stack(&indices).unwrap_err());
+        assert_eq!((), MapFolding::from_stack(&indices).unwrap_err());
     }
 
     #[test]
     fn from_standard_stack_invalid_indices_2() {
         let indices = [0, 1, 2, 3, 4];
-        assert_eq!((), MapFolding::from_standard_stack(&indices).unwrap_err());
+        assert_eq!((), MapFolding::from_stack(&indices).unwrap_err());
     }
 
     #[test]
     fn from_standard_stack_missing_indices() {
         let indices = [5, 3, 1, 2, 1];
-        assert_eq!((), MapFolding::from_standard_stack(&indices).unwrap_err());
+        assert_eq!((), MapFolding::from_stack(&indices).unwrap_err());
     }
 
     #[test]
     fn from_standard_stack_extra_indices() {
         let indices = [5, 3, 4, 5, 2, 1];
-        assert_eq!((), MapFolding::from_standard_stack(&indices).unwrap_err());
+        assert_eq!((), MapFolding::from_stack(&indices).unwrap_err());
     }
 }
