8bbbe12cfb341e4e12dedd92cf67133860089533
msharipov <msharipovr@gmail.com>
2024-11-15 13:43:21 -0500

    feat: implement command line counting utility

--- src/counting.rs
+++ src/counting.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
 use map_folding::MapFolding;
 
-fn brute_force(n: usize) -> usize {
+pub fn brute_force(n: usize) -> usize {
     let perms = (1..(n + 1) as u64).permutations(n);
     let mut counter: usize = 0;
     for p in perms {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 mod counting;
+use clap::{arg, command, value_parser, ArgAction};
 
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .arg(
+            arg!(-u --upto "Count map foldings for all n < N")
+                .required(false)
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            arg!([N] "The number of segments in the map")
+                .required(true)
+                .value_parser(value_parser!(usize)),
+        )
+        .get_matches();
+
+    let n_max: usize = *matches.get_one("N").unwrap();
+    let n_min = if matches.get_flag("upto") { 1 } else { n_max };
+
+    let width = format!("{}", n_max).len() + 1;
+    for i in n_min..n_max + 1 {
+        println!("{:width$} : {}", i, counting::brute_force(i));
+    }
 }
