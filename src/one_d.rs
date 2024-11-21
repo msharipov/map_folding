cdf02178efa7aaa47f78bc6d8fab486455829c49
Maxim Sharipov <msharipovr@gmail.com>
2024-11-20 21:59:00 -0500

    docs: add module description for one_d

--- src/one_d.rs
+++ src/one_d.rs
@@ -1,2 +1,7 @@
+//! This module contains tools for working with one-dimensional map foldings.
+//! (This is also known as the stamp-folding problem.)
+//!
+//! # Reference
+//! [<https://mathworld.wolfram.com/StampFolding.html>]
 pub mod counting;
 pub mod folding;
